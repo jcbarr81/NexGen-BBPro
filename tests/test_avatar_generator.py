ac57708121db41bb9022690dfea883caa7b9db81
jcbarr81 <95983545+jcbarr81@users.noreply.github.com>
2025-09-04 21:54:42 -0400

    Add template-based player avatar generation

--- tests/test_avatar_generator.py
+++ tests/test_avatar_generator.py
@@ -5,9 +5,16 @@ pil_stub = types.ModuleType("PIL")
 pil_stub.Image = object
 sys.modules.setdefault("PIL", pil_stub)
 
-from utils.avatar_generator import _infer_ethnicity
+from utils.avatar_generator import _infer_ethnicity, _select_template
 
 
 def test_infer_ethnicity_known_pairs():
     assert _infer_ethnicity("Frederick Sullivan") == "Anglo"
     assert _infer_ethnicity("Jim Thompson") == "Anglo"
+
+
+def test_select_template_path_resolution():
+    path = _select_template("Asian", "goatee")
+    assert "Template/Asian/goatee.png" in str(path)
+    clean = _select_template("Hispanic", "clean_shaven")
+    assert clean.name == "clean.png"
